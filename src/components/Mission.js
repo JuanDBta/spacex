44dd1b8c1dabd8e1f795afb25cad07a61ba1c268
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-11 16:11:19 +0300

    dispatch an action to store the selected data in Redux store

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,4 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useMemo } from 'react';
+import {
+  fetchmissions, getError, getLoading, getMissions,
+} from '../redux/missions/missionSlice';
+
 function Mission() {
-  return (<div> this is Mission page </div>);
+  const dispatch = useDispatch();
+  const missions = useSelector(getMissions);
+  const missionLoading = useSelector(getLoading);
+  const missionError = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchmissions());
+  }, [dispatch]);
+  // memoize the missions data and use it to render the component on every navigation change.
+  const memoizedMissions = useMemo(() => missions, [missions]);
+  if (missionLoading === true) {
+    return <div>Loading..</div>;
+  }
+  if (missionError !== '') {
+    return <div>error fetching data</div>;
+  }
+  return (
+    <div>
+     
+
+    </div>
+  );
 }
 export default Mission;
