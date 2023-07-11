b25f950948f3438be6326496ac579074fef95665
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-11 13:03:55 -0500

    Created reserved buttons and added functionality to reserved buttons

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getRockets } from '../redux/rockets/rocketsSlice';
+import { getRockets, reserveRocket } from '../redux/rockets/rocketsSlice';
 import styles from '../style/Rockets.module.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
   const isLoading = useSelector((state) => state.rockets.isLoading);
+  const [reservedRocketIds, setReservedRocketIds] = useState([]);
 
   useEffect(() => {
     dispatch(getRockets());
   }, [dispatch]);
 
+  const handleReserveRocket = (rocketId) => {
+    if (reservedRocketIds.includes(rocketId)) {
+      setReservedRocketIds(reservedRocketIds.filter((id) => id !== rocketId));
+    } else {
+      dispatch(reserveRocket(rocketId));
+      setReservedRocketIds([...reservedRocketIds, rocketId]);
+    }
+  };
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className={styles.loading}>Loading...</div>;
   }
 
   return (
@@ -23,8 +33,21 @@ const Rockets = () => {
           <img src={rocket.flickr_images?.[0]} className={styles.image} width="150px" alt={rocket.rocket_name} />
           <div className={styles.content}>
             <h4 className={styles.name}>{rocket.rocket_name}</h4>
-            <p className={styles.description}>{rocket.description}</p>
-            <button type="submit" className={styles.reserve}>Reserve Rocket</button>
+            <p className={styles.description}>
+              {reservedRocketIds.includes(rocket.id) && (
+                <button type="button" className={styles.reserved}>Reserved</button>
+              )}
+              {rocket.description}
+            </p>
+            <button
+              type="button"
+              className={styles.reserve}
+              onClick={() => handleReserveRocket(rocket.id)}
+              disabled={rocket.reserved || reservedRocketIds.includes(rocket.id)}
+              style={reservedRocketIds.includes(rocket.id) ? { backgroundColor: 'white', color: 'rgb(184, 175, 175)', border: '1px rgb(184, 175, 175) solid' } : null}
+            >
+              {reservedRocketIds.includes(rocket.id) ? 'Cancel Reservation' : 'Reserve Rocket'}
+            </button>
           </div>
         </div>
       ))}
