528f76a473b17be664a57087a46f708afe27e230
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-12 17:18:57 +0300

    filter reserved mission for profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import {
+  getMissions,
+} from '../redux/missions/missionSlice';
+
 import styles from '../style/Profile.module.css';
 
 function Profile() {
   const rockets = useSelector((state) => state.rockets.rockets);
 
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-
+  const missions = useSelector(getMissions);
+  const reservedMissions = missions.filter((mission) => mission.reserved);
   return (
     <section className={styles.profilecontainer}>
       <div className={styles.missions}>
         <h2 className={styles.missionstitle}>My Missions</h2>
         <ul className={styles.missionslist}>
-          <li className={styles.launchmission}>Telstar</li>
-          <li className={styles.launchmission}>SES</li>
-          <li className={styles.launchmission}>AsiaSat</li>
-          <li className={styles.launchmission}>ABS</li>
+          {reservedMissions.map((mission) => (
+            <li key={mission.id} className={styles.reservedrocket}>
+              {mission.name}
+            </li>
+          ))}
         </ul>
       </div>
 
