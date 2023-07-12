ea12a11a24e283f5e7f5632767bd002fd0102037
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-11 21:04:00 -0500

    Create html and style to Profile elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../style/nav.css';
+import '../style/Navbar.module.css';
 
 function NAVBAR() {
   return (
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,30 @@
+import React from 'react';
+import styles from '../style/Profile.module.css';
+
 function Profile() {
-  return (<div>this is profile page</div>);
+  return (
+    <section className={styles.profilecontainer}>
+
+      <div className={styles.missions}>
+        <h2 className={styles.missionstitle}>My Missions</h2>
+        <ul className={styles.missionslist}>
+          <li className={styles.launchmission}>Telstar</li>
+          <li className={styles.launchmission}>SES</li>
+          <li className={styles.launchmission}>AsiaSat</li>
+          <li className={styles.launchmission}>ABS</li>
+        </ul>
+      </div>
+
+      <div className={styles.rockets}>
+        <h2 className={styles.rocketstitle}>My Rockets</h2>
+        <ul className={styles.rocketslist}>
+          <li className={styles.reservedrocket}>Falcon 9</li>
+          <li className={styles.reservedrocket}>Falcon Heavy</li>
+          <li className={styles.reservedrocket}>Starship</li>
+        </ul>
+      </div>
+
+    </section>
+  );
 }
 export default Profile;
