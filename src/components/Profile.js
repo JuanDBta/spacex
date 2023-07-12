97cef1d303cd842262d55b5b7d80743e7e3c38cf
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-11 21:18:42 -0500

    Add filter to render reserved rockets

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from '../style/Profile.module.css';
 
 function Profile() {
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
   return (
     <section className={styles.profilecontainer}>
-
       <div className={styles.missions}>
         <h2 className={styles.missionstitle}>My Missions</h2>
         <ul className={styles.missionslist}>
@@ -18,13 +22,15 @@ function Profile() {
       <div className={styles.rockets}>
         <h2 className={styles.rocketstitle}>My Rockets</h2>
         <ul className={styles.rocketslist}>
-          <li className={styles.reservedrocket}>Falcon 9</li>
-          <li className={styles.reservedrocket}>Falcon Heavy</li>
-          <li className={styles.reservedrocket}>Starship</li>
+          {reservedRockets.map((rocket) => (
+            <li key={rocket.id} className={styles.reservedrocket}>
+              {rocket.rocket_name}
+            </li>
+          ))}
         </ul>
       </div>
-
     </section>
   );
 }
+
 export default Profile;
