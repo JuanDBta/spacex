be8ab30c9ddc6286267f099c9aed168684c02f82
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-13 03:07:07 +0300

    add test for profile page

---
+++ src/test/profiletest.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import Profile from '../components/Profile';
+import '@testing-library/jest-dom';
+
+const mockStore = configureStore([]);
+
+describe('Profile component', () => {
+  it('renders reserved rockets and missions', () => {
+    const store = mockStore({
+      rockets: {
+        rockets: [
+          { id: '1', rocket_name: 'Falcon 1', reserved: true },
+          { id: '2', rocket_name: 'Falcon 9', reserved: false },
+        ],
+      },
+      missions: {
+        missions: [
+          { id: '1', name: 'Mission 1', reserved: true },
+          { id: '2', name: 'Mission 2', reserved: false },
+        ],
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <Profile />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Falcon 1')).toBeInTheDocument();
+    expect(screen.getByText('Mission 1')).toBeInTheDocument();
+  });
+});
