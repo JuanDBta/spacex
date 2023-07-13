65aabeee65d1e4e06bd285e9e81f1beac2929089
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-13 21:09:28 +0300

    fix the pressting data issue

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -23,27 +23,28 @@ export const fetchmissions = createAsyncThunk('missions/fetchmission', async () 
   }
 });
 
+export const joinmission = createAsyncThunk('missions/joinmission', async (id) => {
+  try {
+    await fetch(`${url}/${id}`);
+    return id;
+  } catch (error) {
+    return error.message;
+  }
+});
+
+export const leavemission = createAsyncThunk('missions/leavemission', async (id) => {
+  try {
+    await fetch(`${url}/${id}`);
+    return id;
+  } catch (error) {
+    return error.message;
+  }
+});
+
 const missionSlice = createSlice({
   name: 'missions',
   initialState,
-  reducers: {
-    joinmission: (state, action) => {
-      const id = action.payload;
-      const newState = state.missions.map((mission) => {
-        if (mission.id !== id) return mission;
-        return { ...mission, reserved: true };
-      });
-      state.missions = newState;
-    },
-    leavemission: (state, action) => {
-      const id = action.payload;
-      const newState = state.missions.map((mission) => {
-        if (mission.id !== id) return mission;
-        return { ...mission, reserved: false };
-      });
-      state.missions = newState;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchmissions.pending, (state) => {
       state.isloading = true;
@@ -56,12 +57,27 @@ const missionSlice = createSlice({
       state.isloading = false;
       state.error = action.error.message;
     });
+    builder.addCase(joinmission.fulfilled, (state, action) => {
+      const id = action.payload;
+      const newState = state.missions.map((mission) => {
+        if (mission.id !== id) return mission;
+        return { ...mission, reserved: true };
+      });
+      state.missions = newState;
+    });
+    builder.addCase(leavemission.fulfilled, (state, action) => {
+      const id = action.payload;
+      const newState = state.missions.map((mission) => {
+        if (mission.id !== id) return mission;
+        return { ...mission, reserved: false };
+      });
+      state.missions = newState;
+    });
   },
 });
 
 export const getMissions = (state) => state.missions.missions;
 export const getLoading = (state) => state.missions.isloading;
 export const getError = (state) => state.missions.error;
-export const { joinmission, leavemission } = missionSlice.actions;
 
 export default missionSlice.reducer;
